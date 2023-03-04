523c405ef7164d35acb84dfd66ac1ff15d8b548f
harika234 <harikacdm@gmail.com>
2023-03-04 21:44:09 +0530

    patterns

---
+++ InputOutput/src/Suraj.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Suraj {
+    public static void main(String[] args){
+        Scanner suraj = new Scanner(System.in);
+//
+        for(int i=0;i<5;i++){
+            System.out.println("*");
+        }
+    }
+
+
+}
